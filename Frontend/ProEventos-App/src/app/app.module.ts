8eb8d34f468792cabcb7eb54c59708bbb12b3a1d
PauloAlves8039 <pj38alves@gmail.com>
2022-02-26 17:48:32 -0300

    Adicionando o nbx-bootstrap boootstrap e font awesome

--- Frontend/ProEventos-App/src/app/app.module.ts
+++ Frontend/ProEventos-App/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http'
 
 import { AppRoutingModule } from './app-routing.module';
@@ -16,7 +17,8 @@ import { PalestrantesComponent } from './palestrantes/palestrantes.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
