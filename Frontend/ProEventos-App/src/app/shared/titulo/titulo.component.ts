952ff70a056475cdb9081fef8b159ec6cce3981b
PauloAlves8039 <pj38alves@gmail.com>
2022-03-25 20:00:21 -0300

    Atualizando o componente título

--- Frontend/ProEventos-App/src/app/shared/titulo/titulo.component.ts
+++ Frontend/ProEventos-App/src/app/shared/titulo/titulo.component.ts
@@ -8,6 +8,10 @@ import { Component, Input, OnInit } from '@angular/core';
 export class TituloComponent implements OnInit {
 
   @Input() titulo = '';
+  @Input() iconClass = 'fa fa-user';
+  @Input() subtitulo = 'Desde 2022';
+  @Input() botaoListar = false;
+
   constructor() { }
 
   ngOnInit(): void {
