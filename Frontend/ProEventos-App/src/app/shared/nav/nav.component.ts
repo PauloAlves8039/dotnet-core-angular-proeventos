24f40d095c8ca9344358759a893ed6ad4b87954a
PauloAlves8039 <pj38alves@gmail.com>
2022-03-30 19:53:51 -0300

    Definindo o show menu e mini logo

--- Frontend/ProEventos-App/src/app/shared/nav/nav.component.ts
+++ Frontend/ProEventos-App/src/app/shared/nav/nav.component.ts
@@ -1,3 +1,4 @@
+import { Router } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -9,9 +10,13 @@ export class NavComponent implements OnInit {
 
   isCollapsed = true;
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
+  }
+
+  showMenu(): boolean {
+    return this.router.url !== '/user/login';
   }
 
 }
