40b68d76cd7db2f8c8272e2021d0a92ed3b4efaa
PauloAlves8039 <pj38alves@gmail.com>
2022-03-31 21:20:29 -0300

    Atualizando referências no frontend

--- Frontend/ProEventos-App/src/app/components/eventos/evento-lista/evento-lista.component.ts
+++ Frontend/ProEventos-App/src/app/components/eventos/evento-lista/evento-lista.component.ts
@@ -1,10 +1,10 @@
 import { Router } from '@angular/router';
 import { NgxSpinnerService } from 'ngx-spinner';
-import { ToastrService } from 'ngx-toastr';
-import { EventoService } from 'src/app/services/evento.service';
-import { Evento } from 'src/app/models/Evento';
-import { BsModalRef, BsModalService } from 'ngx-bootstrap/modal';
 import { Component, OnInit, TemplateRef } from '@angular/core';
+import { BsModalRef, BsModalService } from 'ngx-bootstrap/modal';
+import { ToastrService } from 'ngx-toastr';
+import { Evento } from '@app/models/Evento';
+import { EventoService } from '@app/services/evento.service';
 
 @Component({
   selector: 'app-evento-lista',
--- Frontend/ProEventos-App/src/app/components/eventos/eventos.component.ts
+++ Frontend/ProEventos-App/src/app/components/eventos/eventos.component.ts
@@ -1,10 +1,4 @@
-import { BsModalRef, BsModalService } from 'ngx-bootstrap/modal';
-import { ToastrService } from 'ngx-toastr';
-import { NgxSpinnerService } from 'ngx-spinner';
-import { Component, OnInit, TemplateRef } from '@angular/core';
-import { Evento } from 'src/app/models/Evento';
-import { EventoService } from 'src/app/services/evento.service';
-
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-eventos',
