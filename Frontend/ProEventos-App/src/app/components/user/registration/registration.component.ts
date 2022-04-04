2232f36fa3c1029a4449ce50b904d9d161ee2bf7
PauloAlves8039 <pj38alves@gmail.com>
2022-04-04 20:31:23 -0300

    Implementando validações para o form registration

--- Frontend/ProEventos-App/src/app/components/user/registration/registration.component.ts
+++ Frontend/ProEventos-App/src/app/components/user/registration/registration.component.ts
@@ -1,15 +1,30 @@
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-registration',
   templateUrl: './registration.component.html',
-  styleUrls: ['./registration.component.scss']
+  styleUrls: ['./registration.component.scss'],
 })
 export class RegistrationComponent implements OnInit {
+  form!: FormGroup;
 
-  constructor() { }
+  constructor(public fb: FormBuilder) {}
+
+  get f(): any { return this.form.controls; }
 
   ngOnInit(): void {
+    this.validation();
   }
 
+  private validation(): void {
+    this.form = this.fb.group({
+      primeiroNome: ['', Validators.required],
+      ultimoNome: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      userName: ['', Validators.required],
+      senha: ['', [Validators.required, Validators.minLength(6)]],
+      confirmeSenha: ['', Validators.required],
+    });
+  }
 }
