825d31f0d7c74312d805294f88374156e74809f6
PauloAlves8039 <pj38alves@gmail.com>
2022-04-12 21:01:02 -0300

    Aplicando ajuste para o botão delete

--- Frontend/ProEventos-App/src/app/components/eventos/evento-lista/evento-lista.component.ts
+++ Frontend/ProEventos-App/src/app/components/eventos/evento-lista/evento-lista.component.ts
@@ -17,6 +17,7 @@ export class EventoListaComponent implements OnInit {
 
   public eventos: Evento[] = [];
   public eventosFiltrados: Evento[] = [];
+  public eventoId = 0;
 
   public larguraImagem = 150;
   public margemImagem = 2;
@@ -71,7 +72,9 @@ export class EventoListaComponent implements OnInit {
     });
   }
 
-  openModal(template: TemplateRef<any>): void {
+  openModal(event: any, template: TemplateRef<any>, eventoId: number): void {
+    event.stopPropagation();
+    this.eventoId = eventoId;
     this.modalRef = this.modalService.show(template, {class: 'modal-sm'});
   }
 
