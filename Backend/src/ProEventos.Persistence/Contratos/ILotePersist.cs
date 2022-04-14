a4f3cfad72c5fcf2538dc354b566a7ee3ed0af19
PauloAlves8039 <pj38alves@gmail.com>
2022-04-14 09:01:33 -0300

    Criando e implementando a interface ILotePersist

---
+++ Backend/src/ProEventos.Persistence/Contratos/ILotePersist.cs
@@ -0,0 +1,11 @@
+using System.Threading.Tasks;
+using ProEventos.Domain.Models;
+
+namespace ProEventos.Persistence.Contratos
+{
+    public interface ILotePersist
+    {
+        Task<Lote[]> GetLotesByEventoIdAsync(int eventoId);
+        Task<Lote> GetLoteByIdsAsync(int eventoId, int id);
+    }
+}
