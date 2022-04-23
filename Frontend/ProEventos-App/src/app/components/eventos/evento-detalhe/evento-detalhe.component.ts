bfb78c942ccba9fd0c57113030f206dd33cf8178
PauloAlves8039 <pj38alves@gmail.com>
2022-04-23 13:57:13 -0300

    Aplicando correção de bugs

--- Frontend/ProEventos-App/src/app/components/eventos/evento-detalhe/evento-detalhe.component.ts
+++ Frontend/ProEventos-App/src/app/components/eventos/evento-detalhe/evento-detalhe.component.ts
@@ -64,7 +64,7 @@ export class EventoDetalheComponent implements OnInit {
   public carregarEvento(): void {
     this.eventoId = +this.activatedRouter.snapshot.paramMap.get('id');
 
-    if (this.eventoId !== null || this.eventoId === 0) {
+    if (this.eventoId !== null && this.eventoId !== 0) {
       this.spinner.show();
 
       this.estadoSalvar = 'put';
@@ -174,9 +174,8 @@ export class EventoDetalheComponent implements OnInit {
   }
 
   public salvarLotes(): void {
-    this.spinner.show();
-
     if (this.form.controls.lotes.valid) {
+      this.spinner.show();
       this.loteService
         .saveLote(this.eventoId, this.form.value.lotes)
         .subscribe(
