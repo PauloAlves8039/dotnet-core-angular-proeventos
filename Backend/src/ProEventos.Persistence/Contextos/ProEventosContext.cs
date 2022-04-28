7f0ebdb1a7860d1ce29b415dc7cbe2526327e7d1
PauloAlves8039 <pj38alves@gmail.com>
2022-04-28 08:31:16 -0300

    Implementando o UserRoles no contexto

--- Backend/src/ProEventos.Persistence/Contextos/ProEventosContext.cs
+++ Backend/src/ProEventos.Persistence/Contextos/ProEventosContext.cs
@@ -23,6 +23,24 @@ namespace ProEventos.Persistence
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<UserRole>(userRole => 
+                {
+                    userRole.HasKey(ur => new { ur.UserId, ur.RoleId});
+
+                    userRole.HasOne(ur => ur.Role)
+                        .WithMany(r => r.UserRoles)
+                        .HasForeignKey(ur => ur.RoleId)
+                        .IsRequired();
+
+                    userRole.HasOne(ur => ur.User)
+                        .WithMany(r => r.UserRoles)
+                        .HasForeignKey(ur => ur.UserId)
+                        .IsRequired();
+                }
+            );
+            
             modelBuilder.Entity<PalestranteEvento>()
                 .HasKey(pe => new {pe.EventoId, pe.PalestranteId});
 
