62f93a883fd611b3a6f667b608f982469bef75fb
PauloAlves8039 <pj38alves@gmail.com>
2022-04-29 20:14:32 -0300

    Implementando a interface IUserPersist

---
+++ Backend/src/ProEventos.Persistence/Contratos/IUserPersist.cs
@@ -0,0 +1,13 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using ProEventos.Domain.Identity;
+
+namespace ProEventos.Persistence.Contratos
+{
+    public interface IUserPersist : IGeralPersist
+    {
+        Task<IEnumerable<User>> GetUsersAsync();
+        Task<User> GetUserByIdAsync(int id);
+        Task<User> GetUserByUserNameAsync(string userName);
+    }
+}
