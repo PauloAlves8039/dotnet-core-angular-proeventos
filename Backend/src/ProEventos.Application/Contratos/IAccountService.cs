069c863e5b19a3a44c7948848cebf2e86d771d5f
PauloAlves8039 <pj38alves@gmail.com>
2022-05-01 12:45:34 -0300

    Implementando a interface IAccountService

--- Backend/src/ProEventos.Application/Contratos/IAccountService.cs
+++ Backend/src/ProEventos.Application/Contratos/IAccountService.cs
@@ -6,8 +6,8 @@ namespace ProEventos.Application.Contratos
 {
     public interface IAccountService
     {
-        Task<bool> UserExists(string username);
-        Task<UserUpdateDto> GetUserByUserNameAsync(string username);
+        Task<bool> UserExists(string userName);
+        Task<UserUpdateDto> GetUserByUserNameAsync(string userName);
         Task<SignInResult> CheckUserPasswordAsync(UserUpdateDto userUpdateDto, string password);
         Task<UserDto> CreateAccountAsync(UserDto userDto);
         Task<UserUpdateDto> UpdateAccount(UserUpdateDto userUpdateDto);
