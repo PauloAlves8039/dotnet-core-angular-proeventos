8e53d3291e683d9c0846874f2dc34856bd526588
PauloAlves8039 <pj38alves@gmail.com>
2022-05-06 18:59:06 -0300

    Criando mapeamento para Usuário

--- Backend/src/ProEventos.Application/Helpers/ProEventosProfile.cs
+++ Backend/src/ProEventos.Application/Helpers/ProEventosProfile.cs
@@ -1,5 +1,6 @@
 using AutoMapper;
 using ProEventos.Application.Dtos;
+using ProEventos.Domain.Identity;
 using ProEventos.Domain.Models;
 
 namespace ProEventos.Application.Helpers
@@ -12,6 +13,10 @@ namespace ProEventos.Application.Helpers
             CreateMap<Lote, LoteDto>().ReverseMap();
             CreateMap<RedeSocial, RedeSocialDto>().ReverseMap();
             CreateMap<Palestrante, PalestranteDto>().ReverseMap();
+
+            CreateMap<User, UserDto>().ReverseMap();
+            CreateMap<User, UserLoginDto>().ReverseMap();
+            CreateMap<User, UserUpdateDto>().ReverseMap();
         }
     }
 }
