c28d4765ac00dbcf6daae16e6152c0d2b47e774e
PauloAlves8039 <pj38alves@gmail.com>
2022-05-08 15:40:40 -0300

    Adicionando o Authorize para Evento

--- Backend/src/ProEventos.Api/Controllers/EventosController.cs
+++ Backend/src/ProEventos.Api/Controllers/EventosController.cs
@@ -2,6 +2,7 @@
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -12,6 +13,7 @@ using ProEventos.Domain.Models;
 
 namespace ProEventos.Api.Controllers
 {
+    [Authorize]
     [ApiController]
     [Route("api/[controller]")]
     public class EventosController : ControllerBase
