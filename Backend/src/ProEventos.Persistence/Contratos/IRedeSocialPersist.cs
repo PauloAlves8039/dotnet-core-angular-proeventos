bdf5f5ea44072fa233776d9a58b9826ce7bf989d
PauloAlves8039 <pj38alves@gmail.com>
2022-05-21 20:11:13 -0300

    Implementando a interface IRedeSocialPersist

---
+++ Backend/src/ProEventos.Persistence/Contratos/IRedeSocialPersist.cs
@@ -0,0 +1,13 @@
+using System.Threading.Tasks;
+using ProEventos.Domain.Models;
+
+namespace ProEventos.Persistence.Contratos
+{
+    public interface IRedeSocialPersist : IGeralPersist
+    {
+        Task<RedeSocial> GetRedeSocialEventoByIdsAsync(int eventoId, int id);
+        Task<RedeSocial> GetRedeSocialPalestranteByIdsAsync(int palestranteId, int id);
+        Task<RedeSocial[]> GetAllByEventoIdAsync(int eventoId);
+        Task<RedeSocial[]> GetAllByPalestranteIdAsync(int palestranteId);
+    }
+}
