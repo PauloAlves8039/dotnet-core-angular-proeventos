6f0ccd9b5423368543a38da8066def19b1e0a41c
PauloAlves8039 <pj38alves@gmail.com>
2022-05-24 20:45:16 -0300

    Implementando  RedesSociaisController e Startup

--- Backend/src/ProEventos.Api/Startup.cs
+++ Backend/src/ProEventos.Api/Startup.cs
@@ -81,13 +81,15 @@ namespace ProEventos.Api
             services.AddScoped<ITokenService, TokenService>();
             services.AddScoped<IAccountService, AccountService>();
             services.AddScoped<IPalestranteService, PalestranteService>();
+            services.AddScoped<IRedeSocialService, RedeSocialService>();
             services.AddScoped<IUtil, Util>();
 
             services.AddScoped<IGeralPersist, GeralPersist>();
             services.AddScoped<IEventoPersist, EventoPersist>();
             services.AddScoped<ILotePersist, LotePersist>();
-
             services.AddScoped<IUserPersist, UserPersist>();
+            services.AddScoped<IPalestrantePersist, PalestrantePersist>();
+            services.AddScoped<IRedeSocialPersist, RedeSocialPersist>();
 
             services.AddCors();
             services.AddSwaggerGen(options =>
