ce081cd8d594a1d91ecac0e64eb5fbfa3fa84bc7
PauloAlves8039 <pj38alves@gmail.com>
2022-05-24 19:56:26 -0300

    Implementação da interface IRedeSocialService

---
+++ Backend/src/ProEventos.Application/Contratos/IRedeSocialService.cs
@@ -0,0 +1,20 @@
+using System.Threading.Tasks;
+using ProEventos.Application.Dtos;
+
+namespace ProEventos.Application.Contratos
+{
+    public interface IRedeSocialService
+    {
+        Task<RedeSocialDto[]> SaveByEvento(int eventoId, RedeSocialDto[] models);
+        Task<bool> DeleteByEvento(int eventoId, int redeSocialId);
+
+        Task<RedeSocialDto[]> SaveByPalestrante(int palestranteId, RedeSocialDto[] models);
+        Task<bool> DeleteByPalestrante(int palestranteId, int redeSocialId);
+
+        Task<RedeSocialDto[]> GetAllByEventoIdAsync(int eventoId);
+        Task<RedeSocialDto[]> GetAllByPalestranteIdAsync(int palestranteId);
+
+        Task<RedeSocialDto> GetRedeSocialEventoByIdsAsync(int eventoId, int RedeSocialId);
+        Task<RedeSocialDto> GetRedeSocialPalestranteByIdsAsync(int PalestranteId, int RedeSocialId);
+    }
+}
